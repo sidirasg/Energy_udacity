f9e5ff340bfa1c464ec29f8940a0c24f27ab24f5
sidirasg <georgesidiras@gmail.com>
2022-04-01 20:23:55 +0300

    Dataset ver0.1

--- intro.py
+++ intro.py
@@ -48,6 +48,7 @@ df3=df.select(col("DateTime"),
      month(col("DateTime")).alias("month"),
     dayofmonth(col("DateTime")).alias("date"),
     hour(col("DateTime")).alias("hour"),
+    minute(col("DateTime")).alias("min"),
   )
 
 
@@ -113,5 +114,57 @@ householdd=spark.createDataFrame(df_household)
 
 #Join two DataSet
 df4=df3.join(householdd, df3.id == householdd.LCLid, 'left')
+df5=df4.withColumn("kwh", df4.kwh.cast('double'))
 
 
+#convert string energy cosumtion data to double
+df5=df4.withColumn("kwh", df4.kwh.cast('double'))
+
+#filter the data to perform to  a smaller sample all the
+dff=df3.filter(df3.id=='MAC000002')
+
+
+#innerjoin with household and energy
+#Apply filter to have better result
+df6=dff.join(householdd, dff.id == householdd.LCLid, 'left')
+#convert string to Chriss
+df7=df6.withColumn("kwh", df6.kwh.cast('double'))
+
+df8=df7.groupby("id","std","year","month","date","hour").sum("kwh")
+
+
+print("Before casting")
+df8.printSchema()
+df8_hours=df8.withColumn("hours",df8['hour'].cast('integer'))
+print("After casting")
+df8_hours.printSchema()
+
+pivot_df = df8_hours.groupby("id","year","month","date").pivot("hours").sum("sum(kwh)")
+
+
+#apply for all data in the dataset the aggregation sums
+
+#first we aggregate in hours , in this data set we have every half hour so we sum every hour
+# In the Eveddent dataset we do not need to aggredate every hour
+df_all8=df5.groupby("id","std","year","month","date","hour").sum("kwh")
+#make ne aggregation per data
+
+
+from pyspark.sql import functions as F
+
+df_all_statistics=df5.groupby("id","std","year","month","date").agg(f.sum("kwh"),f.avg("kwh"),f.max("kwh"),f.min("kwh"),f.count("kwh"),f.stddev_pop("kwh"))
+
+
+print("Before casting")
+df_all8.printSchema()
+
+df_all_hours=df_all8.withColumn("hours",df_all8['hour'].cast('integer'))
+print("After casting")
+df_all_hours.printSchema()
+pivot_df_all=df_all_hours.groupby("id","year","month","date").pivot("hours").sum("sum(kwh)")
+#dataset = pivot_df_all.groupby("id","year","month","date").sum("sum(kwh)").avg("sum(kwh)")("sum(kwh)").min("sum(kwh)").count("sum(kwh)").std("sum(kwh)")
+
+pivot_df_all.count()
+
+
+dataset=pivot_df_all.join(id, pivot_df_all.id == df_all_statistics.id, 'left')
